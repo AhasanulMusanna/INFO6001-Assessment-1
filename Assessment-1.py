4f7ed86b6c858c41a57ad21d846f31c0b9e3f2ca
AhasanulMusanna <ahasanulmusanna@outlook.com>
2025-07-14 10:36:25 +1000

    Initial Commit
    Just pushing the main python file to github to start the assignment

---
+++ Assessment-1.py
@@ -0,0 +1,31 @@
+import hashlib
+import time
+
+class block
+    def __init__(self):
+        self.blocks = []
+
+    def calculate_hash(self):
+
+
+class blockchain
+    def __init__(self):
+        self.blocks = []
+
+    def create_genesis_block(self):
+        genesis_block = block()
+        self.blocks.append(genesis_block)
+
+    def get_latest_block(self):
+        latest_block = self.blocks[-1]
+
+    def add_block(self, block):
+        self.blocks.append(block)
+
+    def proof_of_work(self, block):
+
+    def add_data(self,data):
+
+    def is_chain_valid(self, block):
+
+print("Mining Block....1")
